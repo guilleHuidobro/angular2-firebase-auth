b27f567301fc27f03ed3476c2976daf3596247fb
guilleHuidobro <ghxghxgh@gmail.com>
2017-07-26 16:28:44 -0300

    first commit

---
+++ src/environments/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBVCHLvxNBAY_EaJFYUWnbevGxQlKkjgrU",
+    authDomain: "fir-auth-bcf48.firebaseapp.com",
+    databaseURL: "https://fir-auth-bcf48.firebaseio.com",
+    projectId: "fir-auth-bcf48",
+    storageBucket: "",
+    messagingSenderId: "157197849532"
+  //storageBucket: 'ng2-todo-app.appspot.com'
+};
